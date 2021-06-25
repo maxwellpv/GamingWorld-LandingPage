c2abd9375701b2406a4a70b66d30f9879ccdeca9
Aaron <aaron.plan@hotmail.com>
2021-06-25 11:31:52 -0500

    Bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 
 
 //Cerrar automaticamente el menu(mobile) al hacer click en algun item
-const navLinks = [].slice.call(document.querySelectorAll('.nav-link'))
-const menuToggle = document.getElementById('navbarNavAltMarkup')
 
-var bsCollapse = new bootstrap.Collapse(menuToggle, {
-    toggle: false
-})
+var x = window.matchMedia("(max-width: 990px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
 
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
-})
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        const navLinks = [].slice.call(document.querySelectorAll('.nav-link'))
+        const menuToggle = document.getElementById('navbarNavAltMarkup')
+
+        var bsCollapse = new bootstrap.Collapse(menuToggle, {
+            toggle: false
+        })
+
+        navLinks.forEach((l) => {
+            l.addEventListener('click', () => { bsCollapse.toggle() })
+        })
+    } else {
+    }
+}
+
+
+
